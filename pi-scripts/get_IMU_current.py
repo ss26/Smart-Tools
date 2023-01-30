f2fe803e45f161eafd818c2b8a5937ffb3399d90
ss26 <sundarsripada26@gmail.com>
2023-01-30 14:56:28 -0600

    add: current and mic data

--- pi-scripts/get_IMU_current.py
+++ pi-scripts/get_IMU_current.py
@@ -2,18 +2,19 @@ import logging
 import sys
 import time
 from Adafruit_BNO055 import BNO055
-# import board
-# import busio
-# i2c = busio.I2C(board.SCL, board.SDA)
-# import adafruit_ads1x15.ads1115 as ADS
-# from adafruit_ads1x15.analog_in import AnalogIn
-# ads = ADS.ADS1115(i2c)
-# chan = AnalogIn(ads, ADS.P0)
+import board
+import busio
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
 
 class Current_IMU:
     def __init__(self):
         self.bno = BNO055.BNO055(serial_port='/dev/serial0', rst=18)
-
+        self.i2c = busio.I2C(board.SCL, board.SDA)
+        self.ads = ADS.ADS1115(self.i2c)
+        self.current = AnalogIn(self.ads, ADS.P0)
+        self.mic = AnalogIn(self.ads, ADS.P2)
+        
         if len(sys.argv) == 2 and sys.argv[1].lower() == '-v':
             logging.basicConfig(level=logging.DEBUG)
 
@@ -42,7 +43,8 @@ class Current_IMU:
         bx,by,bz = self.bno.read_magnetometer()
         wx,wy,wz = self.bno.read_gyroscope()
         Accx,Accy,Accz = self.bno.read_accelerometer()
+
         
-        return Accx, Accy, Accz, wx, wy, wz, bx, by, bz
+        return Accx, Accy, Accz, wx, wy, wz, bx, by, bz, self.current.value, self.mic.value
         # print(f'AccX={Accx:.2F} AccY={Accy:.2F} AccZ={Accz:.2F} wx={wx:.2F} wy={wy:.2F} wz={wz:.2F} bx={bx:.2F} by={by:.2F} bz={bz:.2F} heading = {heading:.2F} Roll = {roll:.2F} Pitch = {pitch:.2F}' )
         # print(f'Isens={chan.value:.2F}')
