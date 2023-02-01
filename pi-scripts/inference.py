3a66c1d379055f276a1a3b95e90341ca133973e7
Sundar Sripada V S <sundarsripada26@gmail.com>
2023-01-31 23:25:54 -0600

    Add Raspberry Pi Deployment (#1)
    * add scripts from pi
    
    * fix: data getter
    
    * fix: major refactor, ready to test
    
    * fix: clean up
    
    * fix: raw_df typo
    
    * fix: typos
    
    * fix: _raw_df columns
    
    * fix: add model with 9 sensors
    
    * fix: tensor shape
    
    * fix: y_pred and tensor casting to f32
    
    * add: test dataframe for inference
    
    * fix: relative paths
    
    * fix: modify directory tree
    
    * add: current and mic data
    
    * fix: sensor read tuple
    
    * fix: init raw_df
    
    * add: feedback for time

---
+++ pi-scripts/inference.py
@@ -0,0 +1,41 @@
+import numpy as np
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import os
+from process_data import Preprocess
+import warnings
+import tensorflow as tf
+
+ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
+
+
+warnings.filterwarnings('ignore')
+
+preprocess = Preprocess()
+
+activities = {0: 'Engrave', 1: 'Cut', 2: 'Sand', 3: 'Route'}
+
+tflite_model = ROOT_DIR + '/models/model_11sensors_f32.tflite'
+
+interpreter = tf.lite.Interpreter(tflite_model)
+interpreter.allocate_tensors()
+
+output_intp = interpreter.get_output_details()[0]
+input_intp = interpreter.get_input_details()[0]
+
+num_sensors = 11
+num_features = 10
+
+try:
+    while True:
+        tensor = preprocess.get_tensor()
+        tensor = tf.cast(tensor, tf.float32)
+        tensor = tf.reshape(tensor, (1, num_sensors, num_features))
+        interpreter.set_tensor(input_intp['index'], tensor)
+        interpreter.invoke()
+        y_pred = np.argmax(interpreter.get_tensor(
+            output_intp['index']), axis=1)
+
+        print(f"Predicted Activity: {activities[y_pred[0]]}")
+except KeyboardInterrupt:
+    pass
---
+++ pi-scripts/process_data.py
@@ -0,0 +1,103 @@
+"""
+    Get sensor data from IMU, current sensor, microphone.
+    Perform Pre-processing.
+    Obtain a single pre-processed tensor.
+"""
+import pandas as pd
+import get_IMU_current
+import time
+
+
+try:
+    data_getter = get_IMU_current.Current_IMU()
+except RuntimeError as e:
+    print("Could not start IMU")
+    exit()
+
+
+class Preprocess:
+    """
+    Get live raw data, calculate statistics, output one tensor to send for inference
+    """
+
+    def __init__(self):
+        self.sensors = ['accX', 'accY', 'accZ', 'wx', 'wy',
+                        'wz', 'bx', 'by', 'bz', 'Isens', 'Srms']
+        self.stats = ['min', 'max', 'mean', 'kurt', 'sem',
+                      'std', 'var', 'skew', 'mad', 'sum']
+        self._col_dict = {sensor: None for sensor in self.sensors}
+        self._raw_df = pd.DataFrame([0]*len(self.sensors)).transpose()
+        self._raw_df.columns = self.sensors
+
+        self._processed_df = pd.DataFrame()
+        self._num_features = len(self.stats)
+        self._num_sensors = len(self.sensors)
+        self._tensor = None
+        self._raw_buffer_time = 10        # time of serial buffer data
+        # self._processed_buffer_time = 1     # time for processed data
+
+        # processed_buffer_time = time.perf_counter() + self._processed_buffer_time
+        # while time.perf_counter() <= processed_buffer_time:
+        self.get_raw_df()
+        self.get_processed_df()
+        # print("Done one round of processing!")
+
+    # @staticmethod
+    # def get_statistics(sensor_dict):
+    #     stat_dict = defaultdict(float)
+    #     sensors = [k for k in sensor_dict.keys()]
+    #     stats = ['min', 'max', 'mean', 'kurt', 'sem', 'std', 'var', 'skew', 'mad', 'sum']
+    #     complete_stats = product(sensors, stats)
+
+    def get_raw_df(self):
+        """
+        CREATES
+            One pandas DataFrame containing buffer_time worth raw data
+        """
+
+        buffer_time = time.perf_counter() + self._raw_buffer_time
+        while time.perf_counter() <= buffer_time:
+            accX, accY, accZ, wx, wy, wz, bx, by, bz, isens, mic = data_getter.get_data()
+
+            col_dict = {
+                'accX': accX, 'accY': accY, 'accZ': accZ, 'wx': wx,
+                'wy': wy, 'wz': wz, 'bx': bx, 'by': by, 'bz': bz, 'Isens': isens, 'Srms': mic
+            }
+
+            # add these two after attaching current sensor and mic
+            # isens = np.random.rand(1,)
+            # srms = np.random.rand(1,)
+
+            self._raw_df = pd.concat([self._raw_df, pd.DataFrame(
+                [col_dict.values()], columns=self.sensors)], axis=0, ignore_index=True)
+        print(time.perf_counter() - buffer_time)
+        self._raw_df = self._raw_df.tail(-1)
+        self._raw_df.dropna()
+
+
+    @staticmethod
+    def mad(df: pd.DataFrame):
+        return (df - df.mean()).abs().mean()
+
+    def get_processed_df(self):
+        """
+        CREATES
+            One pandas dataframe row containing the following statistics across self.sensors:
+            - min, max, mean, kurtosis, sem, std, variance, skew, mad, sum
+        """
+
+        assert self._raw_df.size != 0, f"Invalid size for dataframe: {self._raw_df.size}"
+        assert len(
+            self._raw_df.columns) == self._num_sensors, f"Some sensors are missing! Number of sensors detected: {len(self._raw_df.columns)}. Needed {self._num_sensors}!"
+
+        # print(raw_df.describe())
+        stat_df = self._raw_df.agg(
+            ['min', 'max', 'mean', 'kurt', 'sem', 'std', 'var', 'skew', Preprocess.mad, 'sum'])
+#         stat_df.transpose()
+        stat_df = stat_df.transpose()
+
+        self._processed_df = stat_df.unstack().to_frame().T
+        self._processed_df.columns = self._processed_df.columns.map('_'.join)
+
+    def get_tensor(self):
+        return self._processed_df.to_numpy()
