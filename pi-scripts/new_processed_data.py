924f85006cc1fa61181ec3381a0e7517e93b1de7
ss26 <sundarsripada26@gmail.com>
2023-03-06 19:26:33 -0600

    add models

--- pi-scripts/new_processed_data.py
+++ pi-scripts/new_processed_data.py
@@ -20,9 +20,22 @@ def make_processed_df(raw_df, activity):
 
     processed_df = pd.DataFrame()
 
-    for i in tqdm(range(0,3900,10)):
+    # ar
+    # for i in tqdm(range(0,13520,520)):
+    #     _processed_df = pd.DataFrame()
+    #     raw_df_buf = raw_df.iloc[i:i+1040,:]
+    #     stat_df = raw_df_buf.agg(
+    #         ['min', 'max', 'mean', 'kurt', 'sem', 'std', 'var', 'skew', mad, 'sum'])        
+    #     _processed_df = stat_df.unstack().to_frame().T
+    #     _processed_df.columns = _processed_df.columns.map('_'.join)
+    #     processed_df = processed_df.append(_processed_df)
+    # processed_df['Activity'] = activity
+    # return processed_df
+
+    # r-pi
+    for i in tqdm(range(0,8260,295)):
         _processed_df = pd.DataFrame()
-        raw_df_buf = raw_df.iloc[i:i+300,:]
+        raw_df_buf = raw_df.iloc[i:i+590,:]
         stat_df = raw_df_buf.agg(
             ['min', 'max', 'mean', 'kurt', 'sem', 'std', 'var', 'skew', mad, 'sum'])        
         _processed_df = stat_df.unstack().to_frame().T
@@ -31,20 +44,53 @@ def make_processed_df(raw_df, activity):
     processed_df['Activity'] = activity
     return processed_df
 
+# for arduino
+# data_filename = '/home/ss26/Projects/Smart-Tools/data/processed_ar_engraving.csv'
+# data_filename = '/home/ss26/Projects/Smart-Tools/data/processed_ar_cutting.csv'
+# data_filename = '/home/ss26/Projects/Smart-Tools/data/processed_ar_sanding.csv'
+# data_filename = '/home/ss26/Projects/Smart-Tools/data/processed_ar_routing.csv'
 
-data_filename = '/home/ss26/Projects/Smart-Tools/data/processed_engraving.csv'
-# data_filename = '/home/ss26/Projects/Smart-Tools/data/processed_cutting.csv'
-# data_filename = '/home/ss26/Projects/Smart-Tools/data/processed_sanding.csv'
-# data_filename = '/home/ss26/Projects/Smart-Tools/data/processed_routing.csv'
+# df = pd.read_csv('/home/ss26/Projects/Smart-Tools/data/compare_ar_engraving.csv')
+# df = pd.read_csv('/home/ss26/Projects/Smart-Tools/data/compare_ar_cutting.csv')
+# df = pd.read_csv('/home/ss26/Projects/Smart-Tools/data/compare_ar_sanding.csv')
+# df = pd.read_csv('/home/ss26/Projects/Smart-Tools/data/compare_ar_routing.csv')
 
-df = pd.read_csv('/home/ss26/Projects/Smart-Tools/data/reshaped_engraving.csv')
-# df = pd.read_csv('/home/ss26/Projects/Smart-Tools/data/reshaped_cutting.csv')
-# df = pd.read_csv('/home/ss26/Projects/Smart-Tools/data/reshaped_sanding.csv')
-# df = pd.read_csv('/home/ss26/Projects/Smart-Tools/data/reshaped_routing.csv')
-
-proc_df = make_processed_df(df, 0)
+# proc_df = make_processed_df(df, 0)
 # proc_df = make_processed_df(df, 1)
 # proc_df = make_processed_df(df, 2)
 # proc_df = make_processed_df(df, 3)
+
+# for pi
+# data_filename = '/home/ss26/Projects/Smart-Tools/data/processed_pi_engraving.csv'
+# data_filename = '/home/ss26/Projects/Smart-Tools/data/processed_pi_cutting.csv'
+# data_filename = '/home/ss26/Projects/Smart-Tools/data/processed_pi_sanding.csv'
+# data_filename = '/home/ss26/Projects/Smart-Tools/data/processed_pi_routing.csv'
+
+# df = pd.read_csv('/home/ss26/Projects/Smart-Tools/data/compare_pi_engraving.csv')
+# df = pd.read_csv('/home/ss26/Projects/Smart-Tools/data/compare_pi_cutting.csv')
+# df = pd.read_csv('/home/ss26/Projects/Smart-Tools/data/compare_pi_sanding.csv')
+# df = pd.read_csv('/home/ss26/Projects/Smart-Tools/data/compare_pi_routing.csv')
+
+# proc_df = make_processed_df(df, 0)
+# proc_df = make_processed_df(df, 1)
+# proc_df = make_processed_df(df, 2)
+# proc_df = make_processed_df(df, 3)
+
+# for pi conv
+# data_filename = '/home/ss26/Projects/Smart-Tools/data/processed_pi_conv_engraving.csv'
+# data_filename = '/home/ss26/Projects/Smart-Tools/data/processed_pi_conv_cutting.csv'
+# data_filename = '/home/ss26/Projects/Smart-Tools/data/processed_pi_conv_sanding.csv'
+data_filename = '/home/ss26/Projects/Smart-Tools/data/processed_pi_conv_routing.csv'
+
+# df = pd.read_csv('/home/ss26/Projects/Smart-Tools/data/compare_pi_conv_engraving.csv')
+# df = pd.read_csv('/home/ss26/Projects/Smart-Tools/data/compare_pi_conv_cutting.csv')
+# df = pd.read_csv('/home/ss26/Projects/Smart-Tools/data/compare_pi_conv_sanding.csv')
+df = pd.read_csv('/home/ss26/Projects/Smart-Tools/data/compare_pi_conv_routing.csv')
+
+# proc_df = make_processed_df(df, 0)
+# proc_df = make_processed_df(df, 1)
+# proc_df = make_processed_df(df, 2)
+proc_df = make_processed_df(df, 3)
+
 
 proc_df.to_csv(data_filename)
