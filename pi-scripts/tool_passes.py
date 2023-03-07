ff70e8c4c16028173d98b0148b5c2ebd6dc7ba79
ss26 <sundarsripada26@gmail.com>
2023-03-07 01:56:33 -0600

    add: tool passe

--- pi-scripts/tool_passes.py
+++ pi-scripts/tool_passes.py
@@ -60,7 +60,7 @@ try:
         stop_times = stop_times[:start_times_len]
 
     total_times = [x - y for x,y in zip(stop_times, start_times)]
-    text = f"Num Passes = {num_passes}\nAvg. Time/Pass = {sum(total_times)/len(total_times)} s\nAvg. Force = {sum(forces)/len(forces)} lbf\nAvg. Roll Angle = {sum(angles)/len(angles)} deg."
+    text = f"Num Passes = {len(total_times)}\nAvg. Time/Pass = {sum(total_times)/len(total_times)} s\nAvg. Force = {sum(forces)/len(forces)} lbf\nAvg. Roll Angle = {sum(angles)/len(angles)} deg."
     OLED.print_on_OLED(text)
         
         
