351825c4bdcb7e461f1865476603ad65be07f51b
ss26 <sundarsripada26@gmail.com>
2023-03-06 19:59:06 -0600

    test: screen

--- pi-scripts/OLED.py
+++ pi-scripts/OLED.py
@@ -19,7 +19,7 @@ draw = ImageDraw.Draw(image)
 
 # Load a font in 2 different sizes.
 font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 16)
-font2 = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 24)
+font2 = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 28)
 
 offset = 0  # flips between 0 and 32 for double buffering
 print(dir(oled))
@@ -28,7 +28,7 @@ def print_on_OLED(text):
     draw.rectangle((0, 0, oled.width, oled.height * 2), outline=0, fill=0)
     pretext = "Task:"
     draw.text((0, 0), pretext, font=font, fill=255)
-    draw.text((12, 36), text, font=font2, fill=255)
+    draw.text((20, 36), text, font=font2, fill=255)
     oled.image(image)
     oled.show()
     time.sleep(1)
